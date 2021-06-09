4dad78d3b73422fe118bd31309154a643fdf8aca
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-06-08 22:14:22 -0300

    :tada: estudo 08/06

---
+++ JobsCalc/maratona/src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routes = express.Router();
+
+//const basePath = __dirname + "/views"
+const views = __dirname + "/views/"
+
+const profile = {
+    name : "maju",
+    avatar : "https://avatars.githubusercontent.com/u/33471982?v=4",
+    "monthly-budget" : 3000,
+    "days-per-week" : 5,
+    "hours-per-day" : 5,
+    "vacation-per-year" : 4
+}
+
+// req, res
+routes.get('/',(req, res) => res.render(views + "index"))
+routes.get('/job',(req, res) => res.render(views + "job"))
+routes.get('/job/edit',(req, res) => res.render(views + "job-edit"))
+routes.get('/profile',(req, res) => res.render(views + "profile", { profile:profile }))
+
+module.exports = routes;
