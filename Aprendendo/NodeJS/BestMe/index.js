2d3e08fa706804f3021a8769abfa90d7776d2538
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-06-16 18:24:14 -0300

    :tada: estudos 16/06

--- Aprendendo/NodeJS/BestMe/index.js
+++ Aprendendo/NodeJS/BestMe/index.js
@@ -3,8 +3,44 @@
 
 const questions = [
     "O que aprendi hoje?",
-    "O que me deixou aborrecido?",
-    "O que eu poderia fazer para melhorar?",
+    "O que me deixou aborrecido? E o que eu poderia fazer para melhorar?",
     "O que me deixou feliz hoje?",
-    "Quantas pessoas ajudei hoje?"
+    "Quantas pessoas ajudei hoje?",
 ]
+
+const ask = ( index = 0 ) => {
+    process.stdout.write("\n\n" + questions[index] + " > ")
+}
+
+ask()
+
+const answers = []
+process.stdin.on("data", data => {
+    answers.push(data.toString().trim())
+    if (answers.length < questions.length) {
+        index = answers.length
+        ask(index)
+    } else {
+        //console.log(answers)
+        process.exit()
+    }
+})
+
+process.on('exit', () => {
+    console.log(`
+        Legal Maju!
+
+        O que vc aprender hoje foi:
+        ${answers[0]}
+
+        O que te aborreceu e vc poderia melhorar foi:
+        ${answers[1]}
+
+        O que te deivou feliz hoje:
+        ${answers[2]}
+
+        Você ajudou ${answers[3]} pessoas hoje!
+
+        Volte amanhã para novas reflexões!
+    `)
+})
