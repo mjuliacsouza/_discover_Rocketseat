eef0f973db6be418a551296306fa4c0e60918707
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-06-16 12:59:08 -0300

    :tada: estudando 16/06

---
+++ Aprendendo/NodeJS/BestMe/index.js
@@ -0,0 +1,10 @@
+// tipo de console.log()
+//process.stdout.write("Alguma coisa")
+
+const questions = [
+    "O que aprendi hoje?",
+    "O que me deixou aborrecido?",
+    "O que eu poderia fazer para melhorar?",
+    "O que me deixou feliz hoje?",
+    "Quantas pessoas ajudei hoje?"
+]
