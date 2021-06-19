2b2590a14f4488c50213beb519cf4164413dc192
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-06-19 12:57:24 -0300

    :tada: estudos 18/06

--- JobsCalc/maratona/src/routes.js
+++ JobsCalc/maratona/src/routes.js
@@ -6,16 +6,21 @@ const views = __dirname + "/views/"
 
 const profile = {
     name : "maju",
-    avatar : "https://avatars.githubusercontent.com/u/33471982?v=4",
+    avatar : "https://github.com/mjuliacsouza.png",
     "monthly-budget" : 3000,
     "days-per-week" : 5,
     "hours-per-day" : 5,
     "vacation-per-year" : 4
 }
 
+const jobs = []
+
 // req, res
 routes.get('/',(req, res) => res.render(views + "index"))
 routes.get('/job',(req, res) => res.render(views + "job"))
+routes.post('/job',(req, res) => {
+    
+})
 routes.get('/job/edit',(req, res) => res.render(views + "job-edit"))
 routes.get('/profile',(req, res) => res.render(views + "profile", { profile:profile }))
 
